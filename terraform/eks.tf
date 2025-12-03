dc0f6c1bb5d062c053c7c6e2f55705980df15652
HimanM <67066047+HimanM@users.noreply.github.com>
2025-12-04 00:44:48 +0530

    feat: add Terraform configuration for EKS cluster and GitHub Actions CI/CD pipeline for building, pushing images, and updating manifests.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -34,7 +34,7 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.large"]
       capacity_type  = "ON_DEMAND"
 
       iam_role_additional_policies = {
